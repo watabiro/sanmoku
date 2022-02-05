938091d61ba47c052083477ca93d089f2451cee1
KUBA Wataru <wataru.kuba@gmail.com>
2022-02-05 14:53:11 +0900

    change user player

--- user_player.go
+++ user_player.go
@@ -8,27 +8,19 @@ type UserPlayer struct {
 
 func (p UserPlayer) BestMove(b *Board) Move {
 	var file, rank int
-	fmt.Printf("input file: ")
-	_, err := fmt.Scan(&file)
-	if err != nil {
-		file = 0
-	}
+	file = 0
 	for file < 1 || file > 3 {
 		fmt.Printf("input file: ")
-		_, err = fmt.Scan(&file)
+		_, err := fmt.Scan(&file)
 		if err != nil {
 			file = 0
 		}
 	}
-	fmt.Printf("input rank: ")
-	_, err = fmt.Scan(&rank)
-	if err != nil {
-		rank = 0
-	}
+	rank = 0
 	for rank < 1 || rank > 3 {
 		fmt.Printf("input rank: ")
 		fmt.Scan(&rank)
-		_, err = fmt.Scan(&rank)
+		_, err := fmt.Scan(&rank)
 		if err != nil {
 			rank = 0
 		}
